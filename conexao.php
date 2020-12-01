2adfce11f81165a0a2fb1344361b8e708135f101
Eliandro Freitas <eliandroelias@hotmail.com>
2020-12-01 17:46:08 -0300

    autenticação

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $user = "root";
     $pass = "";
-    $db   = "cadastrousuario";
+    $db   = "cadastro2";
 
     $conexao = mysqli_connect($host, $user, $pass, $db);
 ?>
